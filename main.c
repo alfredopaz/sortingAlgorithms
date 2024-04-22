d3f31985da8ef9533ea6fd6d12e53978c612a95e
Alfredo Paz <apazv@unsa.edu.pe>
2024-04-22 18:07:22 -0500

    puntos calculados con 20 repeticiones

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <assert.h>
 #include <string.h>
 #include "sorting.h"
+#include "statistics.h"
+
+#define MAX_REPETITION 20
 
 double gettingSortingTime(int arraySize, int o[], void (*sort)(int, int[])){
   int *a = malloc(sizeof(int) * arraySize);
@@ -24,24 +27,31 @@ double gettingSortingTime(int arraySize, int o[], void (*sort)(int, int[])){
 
 int main(int argc, char* argv[]){
   int arraySize;
-  int* a;
+  int* a; 
+  double data[2][MAX_REPETITION];
   double deltaTimeI, deltaTimeM;
   int minArraySize = atoi(argv[1]);
   int maxArraySize = atoi(argv[2]);
   int incr = atoi(argv[3]);
 
+
   for(arraySize = minArraySize; 
       arraySize < maxArraySize; 
       arraySize += incr){
     a = malloc(sizeof(int) * arraySize);
-    fillArray(arraySize, a);
+    
+    for(int i = 0; i < MAX_REPETITION; i++){
+      fillArray(arraySize, a);
 
-    deltaTimeI = gettingSortingTime(arraySize, a, insertionSort);
-    deltaTimeM = gettingSortingTime(arraySize, a, mergeSort);
+      data[0][i] = gettingSortingTime(arraySize, a, insertionSort);
+      data[1][i] = gettingSortingTime(arraySize, a, mergeSort);
+    }
+
+    deltaTimeI = average(MAX_REPETITION, data[0]);
+    deltaTimeM = average(MAX_REPETITION, data[1]);
 
     printf("%d %f %f\n", arraySize, deltaTimeI, deltaTimeM);
 
     free(a);
   }
-
 }

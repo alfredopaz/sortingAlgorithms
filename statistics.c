7f5f60b9087a69155817de4e015e5722ebd3fe2b
Alfredo Paz <apazv@unsa.edu.pe>
2024-04-23 16:00:26 -0500

    funcion estadistica de desviación estandar agregada

--- statistics.c
+++ statistics.c
@@ -6,3 +6,13 @@ double average(int arraySize, double *data){
     sum += data[i]++;
   return sum / arraySize;
 }
+double standardDeviation(int arraySize, double *a){
+  double mean = average(arraySize, a);
+  double sum = 0;
+  
+  for(int i = 0; i < arraySize; i++)
+    sum += pow(data[i]);
+
+  return sqrt(sum / arraySize);
+}
+
--- statistics.h
+++ statistics.h
@@ -1,4 +1,7 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
+#include <math.h>
+
 double average(int arraySize, double *a);
+double standardDeviation(int arraySize, double *a);
 #endif
